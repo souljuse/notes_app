e90e91fc28f62ab568dd8c8173c8005da71eafcf
Darrenf <darrenf54321@gmail.com>
2016-11-07 15:19:25 +0000

    Failing test for note array

---
+++ note-list-model.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function NoteList() {
+  this.list = [];
+}
+
+NoteList.prototype.addtext = function() {
+  this.list.push("My favourite drink is seltzer")
+}
+
+NoteList.prototype.text = function() {
+  return "My favourite drink is seltzer"
+}
+
+
+function testNoteListModelInstantiation() {
+  var noteList = new NoteList ("My favourite drink is seltzer");
+
+  if (noteList.text()!== "My favourite drink is seltzer") {
+    throw new Error("Incorrect!");
+  }
+
+};
+
+function testNoteIsAddedToArray() {
+  var noteList = new NoteList ("My favourite drink is seltzer");
+  if(noteList.addtext() !== ["My favourite drink is seltzer"]) {
+    throw new Error("Wrong Array!");
+
+  };
+}
+
+testNoteListModelInstantiation();
+testNoteIsAddedToArray();
--- note-model.js
+++ note-model.js
@@ -1,13 +1,16 @@
 'use strict';
 
 function Note() {
-  this.text = "My favourite language is JavaScript";
+}
+
+Note.prototype.text = function() {
+  return "My favourite language is JavaScript"
 }
 
 function testTextofANoteUponInstantiation() {
   var note = new Note("My favourite language is JavaScript");
 
-  if (note.text !== "My favourite language is JavaScript") {
+  if (note.text()!== "My favourite language is JavaScript") {
     throw new Error("Incorrect!");
   }
 };
