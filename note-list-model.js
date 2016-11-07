b736830eb274e15d43336ca80a780e2169e27333
hanrattyjen <hanratty.jen@gmail.com>
2016-11-07 15:58:50 +0000

    List item pushed into list array. Test passes

--- note-list-model.js
+++ note-list-model.js
@@ -4,30 +4,34 @@ function NoteList() {
   this.list = [];
 }
 
-NoteList.prototype.addtext = function() {
-  this.list.push("My favourite drink is seltzer")
-}
-
 NoteList.prototype.text = function() {
-  return "My favourite drink is seltzer"
+  return "My favourite drink is seltzer";
 }
 
+NoteList.prototype.addItem = function() {
+  return this.list.push("My favourite drink is seltzer");
+}
 
 function testNoteListModelInstantiation() {
   var noteList = new NoteList ("My favourite drink is seltzer");
-
   if (noteList.text()!== "My favourite drink is seltzer") {
     throw new Error("Incorrect!");
   }
-
 };
 
 function testNoteIsAddedToArray() {
   var noteList = new NoteList ("My favourite drink is seltzer");
-  if(noteList.addtext() !== "My favourite drink is seltzer") {
+  // console.log(noteList);
+  // console.log(noteList.text());
+  // console.log(noteList.addItem());
+  // console.log(noteList.addItem());
+  // console.log(noteList.addItem());
+  // console.log(noteList.addItem());
+  // console.log(noteList.list);
+  noteList.addItem();
+  if (noteList.list[0] !== "My favourite drink is seltzer") {
     throw new Error("Wrong Array!");
-
-  };
+  }
 }
 
 testNoteListModelInstantiation();
