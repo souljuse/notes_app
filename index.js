2394d24da3dd788db481d8a4a15a51c4810e4ae7
souljuse <oppo.irene@gmail.com>
2016-11-10 18:47:10 +0100

    attempt to use ajax request failed- back to having to refresh page after hashchange

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ noteController.notelist.addNote(note)
 noteController.notelist.addNote(note2)
 notelistview = new NoteListView(noteController.notelist)
 noteController.changeText()
+// document.getElementById("create-note").addEventListener("click", noteController.makeUrlChangeShowNoteForCurrentPage());
+// noteController.makeUrlChangeShowNoteForCurrentPage();
+document.addEventListener("hashchange", noteController.makeUrlChangeShowNoteForCurrentPage());
+
+
+// window.addEventListener("hashchange", function() {
+//
+//   var xhr = new XMLHttpRequest();
+//
+//   // xhr.open('GET', 'noteController.');
+//   xhr.onload = function() {
+//       if (xhr.status === 200) {
+//            console.log("?!")
+//            noteController.makeUrlChangeShowNoteForCurrentPage()
+//          }
+//          else if (xmlhttp.status == 400) {
+//             alert('There was an error 400');
+//          }
+//          else {
+//              alert('something else other than 200 was returned');
+//          }
+//   }
+// console.log(xhr)
+// xhr.send();
+//
+// });
--- note-controller.js
+++ note-controller.js
@@ -15,6 +15,36 @@
     notelist.convertToString()
     notelistview.convertToHtml(notelist);
   };
-  
+
+  NoteController.prototype.makeUrlChangeShowNoteForCurrentPage = function() {
+    // document
+    //       .getElementById("note-list")
+    //       .addEventListener("click", this.showNoteForCurrentPage());
+
+    window.addEventListener("hashchange", this.showNoteForCurrentPage());
+  };
+
+  NoteController.prototype.showNoteForCurrentPage = function() {
+    var noteId = this.getNoteFromUrl(window.location);
+    this.showNote(noteId);
+  };
+
+  NoteController.prototype.getNoteFromUrl = function(location) {
+    return location.hash.split("#")[1];
+  };
+
+  NoteController.prototype.showNote = function(id) {
+    var noteToBeDisplayed = this.notelist.notes[id].text();
+    document
+      .getElementById("app")
+      .innerHTML = noteToBeDisplayed;
+
+
+  };
+
   exports.NoteController = NoteController;
 })(this);
+
+  // function getNoteFromUrl(location){
+  //   location.hash.split("#")[1];
+  // };
